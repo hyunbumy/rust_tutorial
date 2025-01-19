a55c5b1b2e3572a6d2ca5619f65087087b06dda6
Hyun-Bum Yang <yhyunbum@gmail.com>
2025-01-19 16:50:24 -0500

    Deref coercion

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -30,4 +30,61 @@ fn main() {
       Nil,
     }
     let _list: List = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))));
+
+    // Implementing `Deref` trait allows one to customize the bheavior of the
+    // deref operator `*`.  Appropriate implementationg of it allow us to write
+    // code that operates on refs and smart pointers.
+
+    let x: i32 = 5;
+    let y: &i32 = &x;
+    assert_eq!(x, 5);
+    // Must deref the ref to make the comparison
+    assert_eq!(*y, 5);
+
+    let x: i32 = 5;
+    let y: Box<i32> = Box::new(x);
+    assert_eq!(x, 5);
+    // same with using Box
+    assert_eq!(*y, 5);
+
+    // Generic tuple struct
+    use std::ops::Deref;
+    struct MyBox<T>(T);
+    impl<T> MyBox<T> {
+        fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    }
+    // Implement Deref trait for deref behavior using `*`
+    // Without it, Rust can only deref `&` refs.
+    impl<T> Deref for MyBox<T> {
+        // Define an "associated type" for the trait to use
+        type Target = T;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    let x: i32 = 5;
+    let y: MyBox<i32> = MyBox::new(x);
+    assert_eq!(x, 5);
+    // Using `*` here is equivalent to *(y.deref())
+    // Extra `*` is necessary since `deref` returns a ref.
+    assert_eq!(*y, 5);
+
+    // `Deref coercion` converts a ref to a type that implements the Deref trait
+    // into a reference to another type. This happens when we pass a ref to a
+    // ref to a particular type's value as an arg to function or method that
+    // doesn't match the parameter type.
+    fn hello (name: &str) {
+        println!("hello, {name}");
+    }
+    let m: MyBox<String> = MyBox::new(String::from("Rust"));
+    hello(&m);
+    // Deref trait can be used for mutable refs
+    // * From &T to &U when T: Deref<Target=U>
+    // * From &mut T to &mut U when T: DerefMut<Target=U>
+    // * From &mut T to &U when T: Deref<Target=U>
+    // Rust will coerce a mutable ref to immutable, but not the other way
+    // around.
 }
